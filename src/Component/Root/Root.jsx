2f7c4b8c9f9e1aee443f11bfb6431bddb7440031
rezaulkarim <rezaulrahaat@gmail.com>
2025-10-14 12:26:57 +0600

    banner added with inter font integrated

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Home from '../../Pages/Home/Home';
-import Navbar from '../Header/Navbar';
-import Footer from '../Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Home from "../../Pages/Home/Home";
+import Navbar from "../Header/Navbar";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
-    return (
-        <div>
-          <Navbar></Navbar>
-          <Outlet/>
-          <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <div className="bg-gray-100">
+        <Navbar></Navbar>
+
+        <Outlet />
+
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default Root;
